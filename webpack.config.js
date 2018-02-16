c675ff9aa7b2145c554a75a2abbb005c2ee7dd8a
David Morton <davidmortonmail@gmail.com>
2018-02-16 18:37:37 +0000

    Added Battle, Home, Nav, and utils

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,24 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }, 
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader'},
-            { test: /\.(css)$/, use: [ 'style-loader', 'css-loader']}
-        ]
-    },
-    plugins: [ new HtmlWebpackPlugin({
-        template: 'app/index.html'
-    })]
-}
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
